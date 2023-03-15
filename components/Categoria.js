c5bca58fba618a2ea9978650a36383c61023c500
GastonCorimayo <42283804+gec012@users.noreply.github.com>
2023-03-15 16:00:06 -0300

    cambio por jsx

--- components/Categoria.js
+++ components/Categoria.js
@@ -1,11 +1,26 @@
-
+import Image from "next/image";
 
 const Categoria = ({categoria}) =>{
 
     const {nombre, icono, id} = categoria;
 
     return(
-        <div>
+        <div className="flex items-center gap-4 w-full border p-5 hover:bg-amber-400">
+            <Image 
+                width={60}
+                height={60}
+                src={`/assets/img/icono_${icono}.svg`}
+                alt="Imagen icono" 
+            className="mr-5"
+        />
+
+        <button
+        type="button"
+        className="text-2x1 font-bold hover:cursor-pointer"
+        >
+            {nombre}
+
+        </button>
 
         </div>
     )
