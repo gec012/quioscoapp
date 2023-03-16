0b529c9c4b039a43c3479df17fe8d866fc9ab058
GastonCorimayo <42283804+gec012@users.noreply.github.com>
2023-03-15 22:07:34 -0300

    375 completo

--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -6,6 +6,7 @@ const QuioscoContext = createContext()
 
 const QuioscoProvider = ({children}) =>{
   const [categorias, setCategorias] = useState([])
+  const [categoriaActual, setCategoriaActual] = useState ({})
 
   const obtenerCategorias = async () =>{
     const {data} = await axios('/api/categorias')
@@ -15,11 +16,18 @@ const QuioscoProvider = ({children}) =>{
         obtenerCategorias()
    },[])
 
+   const handleClickCategoria = id =>{
+    const categoria = categorias.filter(cate => cate.id === id)
+    setCategoriaActual(categoria[0])
+   }
+
     return(
         <QuioscoContext.Provider
         
           value={{
-             categorias
+             categorias,
+             categoriaActual,
+             handleClickCategoria
            }}
 
         >
