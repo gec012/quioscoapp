51adb5b901d0b6843ef52191eff07527bf22f99f
GastonCorimayo <42283804+gec012@users.noreply.github.com>
2023-03-23 20:29:33 -0300

    finalizado

--- components/Pasos.js
+++ components/Pasos.js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import useQuiosco from "@/hooks/useQuiosco";
+
 
 const pasos = [
   { paso: 1, nombre: "Menu", url: "/" },
@@ -8,28 +8,42 @@ const pasos = [
 ];
 
 const Pasos = () => {
-    
-    const {handleChangePaso, paso}= useQuiosco()
-    const router = useRouter()
-    console.log(paso)
+ 
+  const router = useRouter();
+
+  const calcularProgreso = () => {
+    let valor;
+    if (router.pathname === "/") {
+      valor = 2
+    } else if (router.pathname === "/resumen") {
+      valor = 50
+    } else {
+      valor = 100
+    }
+    return valor;
+  };
+
   return (
     <>
       <div className="flex justify-between mb-5">
         {pasos.map((paso) => (
           <button
-          onClick={()=>{
-            router.push(paso.url);
-            handleChangePaso(paso.paso)
-          }}
-          className="text-2xl font-bold"
-          key={paso.paso}>{paso.nombre}
+            onClick={() => {
+              router.push(paso.url);
+        
+            }}
+            className="text-2xl font-bold"
+            key={paso.paso}
+          >
+            {paso.nombre}
           </button>
         ))}
       </div>
-      <div>
-        <div>
-          
-        </div>
+      <div className="bg-gray-100 mb-10">
+        <div
+          className="rounded-full bg-amber-500 text-xs leading-none h-2 text-center text-white"
+          style={{ width: `${calcularProgreso()}%` }}
+        ></div>
       </div>
     </>
   );
--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,88 +1,136 @@
 import { useState, useEffect, createContext } from "react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import { useRouter } from "next/router";
+
 
 
 const QuioscoContext = createContext()
 
-const QuioscoProvider = ({children}) =>{
+const QuioscoProvider = ({ children }) => {
   const [categorias, setCategorias] = useState([])
-  const [categoriaActual, setCategoriaActual] = useState ({})
+  const [categoriaActual, setCategoriaActual] = useState({})
   const [producto, setProducto] = useState({})
   const [modal, setModal] = useState(false)
   const [pedido, setPedido] = useState([])
-  const [paso, setPaso]= useState(1)
+  const [nombre, setNombre] = useState('')
+  const [total, setTotal] = useState(0)
 
-  const obtenerCategorias = async () =>{
-    const {data} = await axios('/api/categorias')
+
+
+  const router = useRouter()
+
+
+  const obtenerCategorias = async () => {
+    const { data } = await axios('/api/categorias')
     setCategorias(data)
   }
-   useEffect(() =>{
-        obtenerCategorias()
-   },[])
+  useEffect(() => {
+    obtenerCategorias()
+  }, [])
 
-   useEffect(() => {
+  useEffect(() => {
     setCategoriaActual(categorias[0])
-   },[categorias])
+  }, [categorias])
 
+  useEffect(() => {
+    const nuevoTotal = pedido.reduce((total, producto) => (producto.precio * producto.cantidad) + total, 0)
+    setTotal(nuevoTotal)
+  }, [pedido])
 
-   const handleClickCategoria = id =>{
+  const handleClickCategoria = id => {
     const categoria = categorias.filter(cate => cate.id === id)
     setCategoriaActual(categoria[0])
-   }
+    router.push('/')
+  }
 
-   const handleSetProducto = producto =>{
+  const handleSetProducto = producto => {
     setProducto(producto)
-   }
+  }
 
-   const handleChangeModal = () =>{
-     setModal(!modal)
-   }
+  const handleChangeModal = () => {
+    setModal(!modal)
+  }
 
-   const handleAgregarPedido = ({categoriaId, imagen, ...producto})=>{
-    if(pedido.some(productoState => productoState.id === producto.id)){
+  const handleAgregarPedido = ({ categoriaId, ...producto }) => {
+    if (pedido.some(productoState => productoState.id === producto.id)) {
       //actulizar pedido
       const pedidoActualizado = pedido.map(productoAux => productoAux.id === producto.id ? producto : productoAux)
 
       setPedido(pedidoActualizado)
       toast.success('Guardado Correctamente')
-    }else{
-    setPedido([...pedido,producto])
-    toast.success('Agregado al Pedido')
+    } else {
+      setPedido([...pedido, producto])
+      toast.success('Agregado al Pedido')
     }
 
     setModal(false)
-   }
+  }
 
-   const handleChangePaso = paso =>{
-    setPaso(paso)
-   }
+  const handleEditarCantidades = (id) => {
+    const productoActualizar = pedido.filter(producto => producto.id === id)
+
+    setProducto(productoActualizar[0])
+    setModal(!modal)
+  }
+  const handleEliminarProducto = (id) => {
+
+    const pedidoActualizado = pedido.filter(producto => producto.id !== id)
+
+    setPedido(pedidoActualizado)
+
+  }
+  const colocarOrden = async (e) => {
+    e.preventDefault();
+
+    try {
+      await axios.post('/api/ordenes', { pedido, nombre, total, fecha: Date.now().toString() })
+      //resetar la app
+      setCategoriaActual(categorias[0])
+      setPedido([])
+      setNombre('')
+      setTotal(0)
+
+      toast.success('Pedido Realizado Correctamente')
+
+      setTimeout(()=>{
+        router.push('/')
+      },3000)
+
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
 
 
+  return (
+    <QuioscoContext.Provider
 
-    return(
-        <QuioscoContext.Provider
-        
-          value={{
-             categorias,
-             categoriaActual,
-             handleClickCategoria,
-             producto,
-             handleSetProducto,
-             modal,
-             handleChangeModal,
-             handleAgregarPedido,
-             pedido,
-             paso,
-             handleChangePaso
-           }}
+      value={{
+        categorias,
+        categoriaActual,
+        handleClickCategoria,
+        producto,
+        handleSetProducto,
+        modal,
+        handleChangeModal,
+        handleAgregarPedido,
+        pedido,
+        handleEditarCantidades,
+        handleEliminarProducto,
+        nombre,
+        setNombre,
+        colocarOrden,
+        total
+      }}
 
-        >
-            {children}
-        </QuioscoContext.Provider>
-    )
+    >
+      {children}
+    </QuioscoContext.Provider>
+  )
 }
-export{
-    QuioscoProvider
+export {
+  QuioscoProvider
 }
 export default QuioscoContext
